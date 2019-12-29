8f882e1e2a9e6eb1b8ae779c8b86eae515c76015
Ulises <ulises@ulisesceca.com>
2019-12-29 18:44:18 +0100

    added express server for page reload and deep linking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
