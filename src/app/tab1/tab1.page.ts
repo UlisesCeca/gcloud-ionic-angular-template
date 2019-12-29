0256cb2d55d889d5808ab925fcd33cf482b6ace2
Ulises <ulises@ulisesceca.com>
2019-12-29 15:50:12 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://dedede-263318.appspot.com/', { responseType: 'text'}).subscribe((e) => console.log('jejeje ',e))
+  }
 
 }
